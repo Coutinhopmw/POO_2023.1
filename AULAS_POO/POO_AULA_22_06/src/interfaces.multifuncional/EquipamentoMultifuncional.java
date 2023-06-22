218154cc2353e4099f987208b1c65a4952344f9b
Coutinhopmw <cassiocoutinho258@gmail.com>
2023-06-22 15:11:05 -0300

    Metade da aula de POO

---
+++ AULAS_POO/POO_AULA_22_06/src/interfaces.multifuncional/EquipamentoMultifuncional.java
@@ -0,0 +1,21 @@
+public class EquipamentoMultifuncional implements Copiadora, Digitalizadora,Impressora {
+
+    @Override
+    public void imprimir() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'imprimir'");
+    }
+
+    @Override
+    public void digitalizar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'digitalizar'");
+    }
+
+    @Override
+    public void copiar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'copiar'");
+    }
+ 
+}
